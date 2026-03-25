1c86df37b80d8c6779bebd7b768301451ef9d438
Anne-Gaelle <votre-email@example.com>
2026-03-25 14:10:16 +0100

    Revert "style: harmonisation visuelle complete de toutes les pages"
    This reverts commit d91224fd0d16f4161266c623a45738fba88c898d.

--- _find_new_mission.py
+++
@@ -1,6 +0,0 @@
-import io
-path = '/Users/anne-gaelledaval/Downloads/Appli web-2/templates/menu.html'
-lines = io.open(path, 'r', encoding='utf-8').readlines()
-for i, l in enumerate(lines, 1):
-    if 'room-new-mission-badge' in l:
-        print(i, l[:200].rstrip())
